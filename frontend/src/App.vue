eb55068f927d339456ba89d227d3a315b4110ec7
Viransh Shah <shahv47@mcmaster.ca>
2024-07-14 17:14:20 -0400

    chart created, importing values from backend left

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,58 @@
 <template>
-  <div id="app">
-    <ChartComponent />
-  </div>
+  <v-chart class="chart" :option="option" autoresize />
 </template>
 
-<script>
-import ChartComponent from './components/ChartComponent.vue';
+<script setup>
+import { use } from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+import { LineChart } from 'echarts/charts'; // Import LineChart instead of BarChart
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+} from 'echarts/components';
+import VChart, { THEME_KEY } from 'vue-echarts';
+import { ref, provide } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    ChartComponent,
+use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LineChart, // Use LineChart instead of BarChart
+  CanvasRenderer,
+]);
+
+provide(THEME_KEY, 'dark');
+
+const option = ref({
+  title: {
+    text: 'Traffic Sources',
+    left: 'center',
   },
-};
+  tooltip: {
+    trigger: 'axis',
+    formatter: '{a} <br/>{b} : {c} ({d}%)',
+  },
+  grid: { top: 8, right: 8, bottom: 24, left: 36 },
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: 'line',
+      smooth: true,
+    },
+  ],
+});
 </script>
 
-<style>
+<style scoped>
+.chart {
+  height: 100vh;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import ViewUI from 'view-design';
+import ViewUIPlus from 'view-ui-plus';
 import 'view-design/dist/styles/iview.css';
-import ECharts from 'vue-echarts';
-import 'echarts/lib/chart/line';
 
 const app = createApp(App);
 
-app.use(ViewUI);
-app.component('v-chart', ECharts);
-
-app.mount('#app');
+app.use(ViewUIPlus).mount('#app');
