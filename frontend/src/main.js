5f1bf673fcfe2943324609b9c1c6564c00e102e1
Viransh Shah <shahv47@mcmaster.ca>
2024-07-15 18:11:12 -0400

    view-ui-plus/dist/styles/viewuiplus.css" added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import ViewUIPlus from 'view-ui-plus';
-import './index.css'; // Import Tailwind CSS
+import { createApp } from 'vue'
+import App from './App.vue'
+import ViewUIPlus from 'view-ui-plus'
+import './index.css' // Import Tailwind CSS
+import 'view-ui-plus/dist/styles/viewuiplus.css' // Import ViewUI CSS
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(ViewUIPlus).mount('#app');
+app.use(ViewUIPlus).mount('#app')
